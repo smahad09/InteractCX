f7a6533fdbf013f7525df941b9b0cb53b02b0989
smahad09 <mahadsyed8@gmail.com>
2023-06-07 06:32:39 +0500

    Final

--- index.js
+++ index.js
@@ -4,39 +4,34 @@ const bodyParser = require('body-parser');
 const formatDate = require('./formatDate');
 require('dotenv').config();
 
+
 const port = process.env.PORT;
 const app = express();
 const baseURL = 'https://orderstatusapi-dot-organization-project-311520.uc.r.appspot.com/api/getOrderStatus';
 
+
 app.use(bodyParser.json());
-
-
-app.get('/', (request,response)=> {
-    response.send('App is running!');
-});
-
 
 app.post('/', async (request,response)=> {
     const {queryResult} = request.body;
-    
+
     const orderId = queryResult.parameters.o2;
 
     const result = await axios.post(baseURL, {orderId: orderId});
 
     var shipmentDate = result.data.shipmentDate;
-    console.log(shipmentDate);
     shipmentDate = formatDate(shipmentDate);
     console.log(shipmentDate);
 
     const res = {
         fulfillmentText: `Your order ${orderId} will be shipped on ${shipmentDate}`,
-        fulfillmentMessages: [
-          { text: {
+        fulfillmentMessages: [{ 
+            text: {
                 text: [`Your order ${orderId} will be shipped on ${shipmentDate}`]
             }
-          }
-        ]
+        }]
     };
+
     response.json(res);
 });
 
